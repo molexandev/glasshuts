50be7850561fa642a261dcdf4cd5ea96d0c05585
molexandev01~ <molexandev@gmail.com>
2023-06-25 17:42:01 +0300

    added home page

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,15 @@
-$(function(){
-   
-})
+$(function () {
+
+    $(".top-slider__inner").slick({
+        dots: true,
+        arrows: true,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        draggable: true,
+      //   variableWidth: true,
+      //   adaptiveHeight: true
+    });
+
+});
