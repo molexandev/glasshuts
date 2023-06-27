fa996f23f50e547d11f7fb74996da9679bb628a5
molexandev01~ <molexandev@gmail.com>
2023-06-27 20:21:43 +0300

    start booking page

--- app/js/main.js
+++ app/js/main.js
@@ -13,16 +13,27 @@ $(function () {
     // page-slider-left
 
     $(".slider-page__slider-inner").slick({
-      dots: true,
-      arrows: true,
-      infinite: false,
-      speed: 1000,
-      autoplay: true,
-      autoplaySpeed: 2000,
-      fade: true,
-      draggable: true,
-  });
-    
-    
+        dots: true,
+        arrows: true,
+        infinite: false,
+        speed: 1000,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        fade: true,
+        draggable: true,
+    });
+
+    // BOOKING SLIDE
+
+    $(".choose-services__label").on("click", function () {
+      $(".choose-services__input").toggleClass("choose-services__input--active");
+      $(this).next().slideToggle();
+    });
+
+
+
+  //   $(".asdasdasdas").on("click", function () {
+  //     $('.asdasdasdas').toggleClass("choose-services__label-77777777777");
+  // });
 
 });
